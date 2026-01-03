d3a1ba5a5bffbed6fc7488976a58cf92595c15b2
rameshpunyamanthula <punyamanthularamesh53@gmail.com>
2026-01-03 15:05:50 +0530

    Setup index.jsx componenet

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import App from "./App";
 import "./styles/global.css"; // Import global styles
-
+{}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
