edbab8d273b3e81ecaf0dfa95a77cb439a313558
rameshpunyamanthula <punyamanthularamesh53@gmail.com>
2026-01-03 15:07:58 +0530

    Setup webpack for frontend

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,12 +3,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.jsx',
+
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/',
   },
+
   mode: 'development',
+
   module: {
     rules: [
       {
@@ -24,17 +27,32 @@ module.exports = {
       },
     ],
   },
+
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
   ],
-  devServer: {
+
+ devServer: {
   historyApiFallback: true,
-  port: 3001,
+  port: 5173,          // keep same port you are using
   hot: true,
+  open: true,
+
+  proxy: {
+    "/api": {
+      target: "http://localhost:5000",
+      changeOrigin: true,
+      secure: false,
+    },
+  },
 },
+
+
+
 };
